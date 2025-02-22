7eb85d6cef268ed0b5fbe10d759e86e12437cad2
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-02-22 15:20:06 +0530

    initialize frontend with React and Vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Login from "./pages/Login.jsx";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+};
+
+export default App;
